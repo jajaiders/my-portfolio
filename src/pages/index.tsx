24269279c91c3352764dddb2912901be47f5a179
jajaiders <jajaiders@gmail.com>
2025-03-20 14:18:58 -0500

    First commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
-    return (
-      <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold">¡Bienvenido a mi Portafolio!</h1>
-        <p className="text-lg text-gray-600 mt-2">Explora mis proyectos y habilidades.</p>
-      </main>
-    );
-  }
-  
+  return (
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">¡Welcome to my Portfolio!</h1>
+      <p className="text-lg text-gray-600 mt-2">
+        Explora mis proyectos y habilidades.
+      </p>
+    </main>
+  );
+}
