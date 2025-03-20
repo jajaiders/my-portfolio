775a94a43187712232a9f7ac7cba759cd391288a
jajaiders <jajaiders@gmail.com>
2025-03-20 14:24:31 -0500

    Fisrt commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">¡Welcome to my Portfolio!</h1>
+      <h1 className="text-4xl font-bold">¡Bienvenido a mi portafolio!</h1>
       <p className="text-lg text-gray-600 mt-2">
         Explora mis proyectos y habilidades.
       </p>
