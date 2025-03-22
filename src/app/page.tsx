56dcbd441db5af3cda596c905727ab5c3804355b
jajaiders <jajaiders@gmail.com>
2025-03-22 16:59:55 -0500

    Adding the base about me section and base project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
 import avatar from "@/assets/avatar.webp";
 import Link from "next/link";
+import javaScriptLogo from "@/assets/logos/javaScript-logo.svg";
+import nodejsLogo from "@/assets/logos/nodejs-logo.svg";
+import reactLogo from "@/assets/logos/react-logo.svg";
+import tailwindLogo from "@/assets/logos/tailwind-logo.svg";
+import mongodbLogo from "@/assets/logos/mongoDB-logo.svg";
+import nextjsLogo from "@/assets/logos/nextjs-logo.svg";
+import postgresSQLLogo from "@/assets/logos/postgresql-logo.svg";
+import awsLogo from "@/assets/logos/aws-logo.svg";
 import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      <div>
+    <main className="h-full py-30">
+      <div className="h-full container mx-auto">
         {/* Hero section */}
-        <div className="flex items-center justify-between mx-auto h-[700px] container">
+        <div className="flex items-center justify-between">
           <div
             id="header"
             className="flex flex-col gap-2 max-w-[50%] h-full justify-center "
@@ -40,14 +49,63 @@ export default function Home() {
             <Image
               src={avatar}
               alt="Picture of the author"
-              className="h-full w-full"
+              className="h-full w-full bg-blue-600 rounded-[50%]"
             />
           </div>
         </div>
         {/* About me section  */}
-        <div></div>
+        <div>
+          <div className="flex py-30 gap-30 justify-center">
+            <div>
+              <Image
+                src={avatar}
+                height={250}
+                alt="yo"
+                className="bg-blue-600 rounded-[50%]"
+              />
+            </div>
+            <div className="w-[50%]">
+              <h2 className="text-5xl pb-5">Sobre mi</h2>
+              <p className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quibusdam aut repellat quis. Molestias optio commodi natus
+                explicabo nostrum dignissimos non nulla reiciendis recusandae
+                quidem voluptatibus maiores ducimus, ratione corrupti soluta.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center flex-col gap-15">
+            <h2 className="text-5xl">Conocimientos</h2>
+            <div className="flex flex-row gap-5">
+              <Image src={javaScriptLogo} height={60} alt="logo" />
+              <Image src={reactLogo} height={60} alt="logo" />
+              <Image src={tailwindLogo} height={60} alt="logo" />
+              <Image src={nodejsLogo} height={60} alt="logo" />
+              <Image src={mongodbLogo} height={60} alt="logo" />
+            </div>
+            <h3 className="text-3xl">Aprendiendo</h3>
+            <div className="flex flex-row gap-5">
+              <Image src={nextjsLogo} height={60} alt="logo" />
+              <Image src={postgresSQLLogo} height={60} alt="logo" />
+              <Image src={awsLogo} height={60} alt="logo" />
+            </div>
+          </div>
+        </div>
         {/* Projects section */}
-        <div></div>
+        <div className="flex flex-col gap-5 items-center py-30">
+          <h2 className="text-5xl mb-10">Proyectos</h2>
+          <div className="flex w-[100%] justify-center gap-2">
+            <Card className="w-60 h-36 flex justify-center items-center">
+              <Image src={javaScriptLogo} height={100} alt="project image"/>
+            </Card>
+            <Card className="w-60 h-36 flex justify-center items-center">
+              <Image src={javaScriptLogo} height={100} alt="project image"/>
+            </Card>
+            <Card className="w-60 h-36 flex justify-center items-center">
+              <Image src={javaScriptLogo} height={100} alt="project image"/>
+            </Card>
+          </div>
+        </div>
         {/* Contact section */}
         <div></div>
       </div>
