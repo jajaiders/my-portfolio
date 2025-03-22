ee2c493c00911fa3ef61b14a9ae486d91de83814
jajaiders <jajaiders@gmail.com>
2025-03-22 17:19:28 -0500

    Fixing the 'Project' and 'contact me' button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,100 +14,103 @@ import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="h-full py-30">
-      <div className="h-full container mx-auto">
-        {/* Hero section */}
-        <div className="flex items-center justify-between">
-          <div
-            id="header"
-            className="flex flex-col gap-2 max-w-[50%] h-full justify-center "
-          >
-            <h3 className="text-4xl">Hola!</h3>
-            <h1 className="text-6xl">
-              Soy <span className="text-blue-400 text-7xl">Jaider Ramirez</span>
-              , <br /> Desarrollador de Sorftware
-            </h1>
-            <span className="text-lg text-gray-600 w-[75%] pt-5">
-              Apasionado por la creación de soluciones digitales eficientes,
-              seguras y escalables. Especializado en el desarrollo de
-              aplicaciones web modernas.
-            </span>
-            <div className="flex pt-9 gap-4">
-              <Link href="/projects" className="hover:cursor-pointer">
-                <Button className="text-2xl h-12 w-44 pb-2.5 px-5 rounded-3xl border">
-                  Proyectos
-                </Button>
-              </Link>
-              <Link href="/contact" className="hover:cursor-pointer">
-                <Button className="bg-blue-500 hover:bg-blue-600 text-white text-2xl h-12 w-44 pb-2.5 px-5 rounded-3xl">
-                  Contactame
-                </Button>
-              </Link>
+    <main>
+      <div className="h-full py-30">
+        <div className="h-full container mx-auto">
+          {/* Hero section */}
+          <div className="flex items-center justify-between">
+            <div
+              id="header"
+              className="flex flex-col gap-2 max-w-[50%] h-full justify-center "
+            >
+              <h3 className="text-4xl">Hola!</h3>
+              <h1 className="text-6xl">
+                Soy{" "}
+                <span className="text-blue-400 text-7xl">Jaider Ramirez</span>
+                , <br /> Desarrollador de Sorftware
+              </h1>
+              <span className="text-lg text-gray-600 w-[75%] pt-5">
+                Apasionado por la creación de soluciones digitales eficientes,
+                seguras y escalables. Especializado en el desarrollo de
+                aplicaciones web modernas.
+              </span>
+              <div className="flex pt-9 gap-4">
+                <Link href="/projects" className="rounded-3xl">
+                  <Button className="text-2xl h-12 w-44 pb-2.5 px-5 cursor-pointer rounded-3xl border border-blue-500 hover:bg-blue-100">
+                    Proyectos
+                  </Button>
+                </Link>
+                <Link href="/contact" className="rounded-3xl">
+                  <Button className="bg-blue-500 hover:bg-blue-600 cursor-pointer text-white text-2xl h-12 w-44 pb-2.5 px-5 rounded-3xl">
+                    Contactame
+                  </Button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div id="Image" className="h-full flex items-center pl-20">
-            <Image
-              src={avatar}
-              alt="Picture of the author"
-              className="h-full w-full bg-blue-600 rounded-[50%]"
-            />
-          </div>
-        </div>
-        {/* About me section  */}
-        <div>
-          <div className="flex py-30 gap-30 justify-center">
-            <div>
+            <div id="Image" className="h-full flex items-center pl-20">
               <Image
                 src={avatar}
-                height={250}
-                alt="yo"
-                className="bg-blue-600 rounded-[50%]"
+                alt="Picture of the author"
+                className="h-full w-full bg-blue-600 rounded-[50%]"
               />
             </div>
-            <div className="w-[50%]">
-              <h2 className="text-5xl pb-5">Sobre mi</h2>
-              <p className="text-xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Quibusdam aut repellat quis. Molestias optio commodi natus
-                explicabo nostrum dignissimos non nulla reiciendis recusandae
-                quidem voluptatibus maiores ducimus, ratione corrupti soluta.
-              </p>
+          </div>
+          {/* About me section  */}
+          <div>
+            <div className="flex py-30 gap-30 justify-center">
+              <div>
+                <Image
+                  src={avatar}
+                  height={250}
+                  alt="yo"
+                  className="bg-blue-600 rounded-[50%]"
+                />
+              </div>
+              <div className="w-[50%]">
+                <h2 className="text-5xl pb-5">Sobre mi</h2>
+                <p className="text-xl">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Quibusdam aut repellat quis. Molestias optio commodi natus
+                  explicabo nostrum dignissimos non nulla reiciendis recusandae
+                  quidem voluptatibus maiores ducimus, ratione corrupti soluta.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center flex-col gap-15">
+              <h2 className="text-5xl">Conocimientos</h2>
+              <div className="flex flex-row gap-5">
+                <Image src={javaScriptLogo} height={60} alt="logo" />
+                <Image src={reactLogo} height={60} alt="logo" />
+                <Image src={tailwindLogo} height={60} alt="logo" />
+                <Image src={nodejsLogo} height={60} alt="logo" />
+                <Image src={mongodbLogo} height={60} alt="logo" />
+              </div>
+              <h3 className="text-3xl">Aprendiendo</h3>
+              <div className="flex flex-row gap-5">
+                <Image src={nextjsLogo} height={60} alt="logo" />
+                <Image src={postgresSQLLogo} height={60} alt="logo" />
+                <Image src={awsLogo} height={60} alt="logo" />
+              </div>
             </div>
           </div>
-          <div className="flex items-center flex-col gap-15">
-            <h2 className="text-5xl">Conocimientos</h2>
-            <div className="flex flex-row gap-5">
-              <Image src={javaScriptLogo} height={60} alt="logo" />
-              <Image src={reactLogo} height={60} alt="logo" />
-              <Image src={tailwindLogo} height={60} alt="logo" />
-              <Image src={nodejsLogo} height={60} alt="logo" />
-              <Image src={mongodbLogo} height={60} alt="logo" />
-            </div>
-            <h3 className="text-3xl">Aprendiendo</h3>
-            <div className="flex flex-row gap-5">
-              <Image src={nextjsLogo} height={60} alt="logo" />
-              <Image src={postgresSQLLogo} height={60} alt="logo" />
-              <Image src={awsLogo} height={60} alt="logo" />
+          {/* Projects section */}
+          <div className="flex flex-col gap-5 items-center py-30">
+            <h2 className="text-5xl mb-10">Proyectos</h2>
+            <div className="flex w-[100%] justify-center gap-2">
+              <Card className="w-60 h-36 flex justify-center items-center">
+                <Image src={javaScriptLogo} height={100} alt="project image" />
+              </Card>
+              <Card className="w-60 h-36 flex justify-center items-center">
+                <Image src={javaScriptLogo} height={100} alt="project image" />
+              </Card>
+              <Card className="w-60 h-36 flex justify-center items-center">
+                <Image src={javaScriptLogo} height={100} alt="project image" />
+              </Card>
             </div>
           </div>
+          {/* Contact section */}
+          <div></div>
         </div>
-        {/* Projects section */}
-        <div className="flex flex-col gap-5 items-center py-30">
-          <h2 className="text-5xl mb-10">Proyectos</h2>
-          <div className="flex w-[100%] justify-center gap-2">
-            <Card className="w-60 h-36 flex justify-center items-center">
-              <Image src={javaScriptLogo} height={100} alt="project image"/>
-            </Card>
-            <Card className="w-60 h-36 flex justify-center items-center">
-              <Image src={javaScriptLogo} height={100} alt="project image"/>
-            </Card>
-            <Card className="w-60 h-36 flex justify-center items-center">
-              <Image src={javaScriptLogo} height={100} alt="project image"/>
-            </Card>
-          </div>
-        </div>
-        {/* Contact section */}
-        <div></div>
       </div>
     </main>
   );
